9cd143762b10356bb7cef5d9eda94c338685b0aa
Eugene Vilensky <evilensky@gmail.com>
2014-07-01 13:44:37 -0500

    options

--- parse_disk.rb
+++ parse_disk.rb
@@ -3,23 +3,41 @@
 require 'slop'
 require 'nokogiri'
 
+
+#get some options.  cool stuff like usernames and passwords can go in here
+
+
 opts = Slop.parse do
   banner 'xml_to_csv.rb [options] --filename xxxx --channel yyyy'
   on 'filename=', 'filename'
   on 'channel=', 'channel'
 end
 
-file = File.new(opts[:filename])
-doc  = Nokogiri::XML file
+#we're only going to load one file at a time.
 
+
+
+def load_file(filename, opts)
+  file = File.new(opts[:filename])
+  doc  = Nokogiri::XML file
+  return doc
+end
+
+#within the downloaded XML file, 
 subnodes = doc.xpath("//item")
 
+def print_subnodes(subnodes, opts)
 
+  subnodes.each do | node |
+    datetime = node.xpath("datetime").text
+    channel = node.xpath("#{opts[:channel]}").text
+    date_str = datetime.to_s
 
-subnodes.each do | node |
-  datetime = node.xpath("datetime").text
-  channel = node.xpath("#{opts[:channel]}").text
-  date_str = datetime.to_s
+    print date_str[0..8] + ',' + channel + "\n"
+  end
 
-  print date_str[0..8] + ',' + channel + "\n"
 end
+
+load_file(filename, opts)
+
+print_subnodes(subnodes, opts)
